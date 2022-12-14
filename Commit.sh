b79a9544917ab5a4385d652094457d6dd082543f
Shiyo <strikinginsanity@hotmail.com>
2022-12-14 10:15:18 -0500

    generic

---
+++ Commit.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+cd "C:/Ashita 4/addons/treasurepool"
+git add .
+git commit -m "generic"
+git push origin main
---
+++ Pull.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+cd "C:/Ashita 4/addons/treasurepool"
+git reset --hard HEAD
+git clean -fxd
+git pull
